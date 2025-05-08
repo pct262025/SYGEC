602a2dc3f396caef9a7661dadad8c3e643215d9e
AYMAR <akoeteaymar58@gmail.com>
2025-05-08 14:24:25 +0000

    i

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$action = isset($_GET['action']) ? $_GET['action'] : 'accueil';
+
+switch ($action) {
+    case 'accueil':
+        require_once 'controllers/HomeController.php';
+        $controller = new HomeController();
+        $controller->index();
+        break;
+}
