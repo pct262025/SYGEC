e3044df71f4c166176c63de131c93fcbf5cf0e51
Junior-Amian <123127165+Junior-Amian@users.noreply.github.com>
2025-05-14 20:51:43 +0000

    Update home2.php

--- views/home2.php
+++ views/home2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil </title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="home2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
